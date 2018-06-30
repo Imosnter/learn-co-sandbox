e67e3c4cb940263d89e361a9472ebcf80402eb69
Emmanuel D Mendez <manche65@gmail.com>
2018-06-30 04:57:33 +0000

    automatically backed up by learn

---
+++ learn.js
@@ -0,0 +1,55 @@
+// var word
+// word ='bird'
+
+// console.log(word)
+
+// word = 'dog'
+// console.log(word)
+
+// function sayHello(){
+//   console.log('Hello!')
+// }
+
+function speaker(){
+  sentence = 'Bird is the word';
+  // console.log(sentence);
+}
+speaker();
+console.log(sentence);
+
+var cuteAnimal = 'quokka';
+ 
+function makeVariable() {
+  var cuteAnimal = 'sugar glider';
+  console.log(cuteAnimal);
+  return cuteAnimal;
+}
+makeVariable();
+// 'sugar glider'
+ 
+cuteAnimal;
+console.log(cuteAnimal);
+// 'quokka'
+
+volume =10;
+
+function returnEleven(){
+  var volume = 11;
+  console.log(volume);
+  return volume;
+}
+
+returnEleven();
+
+volume;
+console.log(volume);
+
+function goToEleven(){
+  volume =11;
+  return volume;
+}
+
+goToEleven();
+console.log(volume)
+
+volume;
